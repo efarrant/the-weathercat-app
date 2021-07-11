e2caf7cd5421ce60bc3123f7a2b595a129f49e21
efarrant <83728803+efarrant@users.noreply.github.com>
2021-07-11 18:44:14 +0100

    Duplicate function deleted

--- src/app.js
+++ src/app.js
@@ -36,53 +36,53 @@ function displayCity(event) {
   let searchInput = document.querySelector("#input-city");
   let city = document.querySelector(".city-and-country");
   city.innerHTML = `${searchInput.value}`;
-  // let apiKey = "4d25fa39d39f9d90630ef9306854c84f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&units=metric&appid=${apiKey}`;
   axios.get(`${apiUrl}`).then(showTemperature);
+}
 
-  function showTemperature(response) {
-    let temperatureNumber = Math.round(response.data.main.temp);
-    let weatherDescription = response.data.weather[0].description;
-    let windSpeed = Math.round(response.data.wind.speed);
-    let humidity = response.data.main.humidity;
-    let temperatureElement = document.querySelector(".temperature");
-    let weatherDescriptionElement =
-      document.querySelector(".weather-condition");
-    let dateElement = document.querySelector("#date");
-    let iconElement = document.querySelector("#icon");
-    let windElement = document.querySelector("#windspeed");
-    let humidityElement = document.querySelector("#humidity");
-    temperatureElement.innerHTML = temperatureNumber;
-    weatherDescriptionElement.innerHTML = weatherDescription;
-    windElement.innerHTML = windSpeed;
-    humidityElement.innerHTML = humidity;
-    dateElement.innerHTML = formatDate(response.data.dt * 1000);
-    iconElement.setAttribute(
-      "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-    getForecast(response.data.coord);
-    if (hours > 7 && hours < 21) {
-      if (weatherDescriptionElement.innerHTML.includes("ain")) {
-        document.getElementById("backgroundImage").style.backgroundImage =
-          "url(src/gifs/window-cat-rain.gif)";
-      } else if (weatherDescriptionElement.innerHTML.includes("sun")) {
-        document.getElementById("backgroundImage").style.backgroundImage =
-          "url(src/gifs/window-cat-sunny.gif)";
-      } else if (weatherDescriptionElement.innerHTML.includes("cloud")) {
-        document.getElementById("backgroundImage").style.backgroundImage =
-          "url(src/gifs/window-cat-clouds.gif)";
-      } else if (weatherDescriptionElement.innerHTML.includes("storm")) {
-        document.getElementById("backgroundImage").style.backgroundImage =
-          "url(src/gifs/window-cat-thunderstorm.gif)";
-      } else if (weatherDescriptionElement.innerHTML.includes("snow")) {
-        document.getElementById("backgroundImage").style.backgroundImage =
-          "url(src/gifs/window-cat-snow.gif)";
-      } else if (weatherDescriptionElement.innerHTML.includes("wind")) {
-        document.getElementById("backgroundImage").style.backgroundImage =
-          "url(src/gifs/window-cat-windy.gif)";
-      }
-      
+function showTemperature(response) {
+  let temperatureNumber = Math.round(response.data.main.temp);
+  let weatherDescription = response.data.weather[0].description;
+  let windSpeed = Math.round(response.data.wind.speed);
+  let humidity = response.data.main.humidity;
+  let temperatureElement = document.querySelector(".temperature");
+  let weatherDescriptionElement = document.querySelector(".weather-condition");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  let windElement = document.querySelector("#windspeed");
+  let humidityElement = document.querySelector("#humidity");
+  temperatureElement.innerHTML = temperatureNumber;
+  weatherDescriptionElement.innerHTML = weatherDescription;
+  windElement.innerHTML = windSpeed;
+  humidityElement.innerHTML = humidity;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  getForecast(response.data.coord);
+  if (hours > 7 && hours < 21) {
+    if (weatherDescriptionElement.innerHTML.includes("ain")) {
+      document.getElementById("backgroundImage").style.backgroundImage =
+        "url(src/gifs/window-cat-rain.gif)";
+    } else if (weatherDescriptionElement.innerHTML.includes("sun")) {
+      document.getElementById("backgroundImage").style.backgroundImage =
+        "url(src/gifs/window-cat-sunny.gif)";
+    } else if (weatherDescriptionElement.innerHTML.includes("cloud")) {
+      document.getElementById("backgroundImage").style.backgroundImage =
+        "url(src/gifs/window-cat-clouds.gif)";
+    } else if (weatherDescriptionElement.innerHTML.includes("storm")) {
+      document.getElementById("backgroundImage").style.backgroundImage =
+        "url(src/gifs/window-cat-thunderstorm.gif)";
+    } else if (weatherDescriptionElement.innerHTML.includes("snow")) {
+      document.getElementById("backgroundImage").style.backgroundImage =
+        "url(src/gifs/window-cat-snow.gif)";
+    } else if (weatherDescriptionElement.innerHTML.includes("wind")) {
+      document.getElementById("backgroundImage").style.backgroundImage =
+        "url(src/gifs/window-cat-windy.gif)";
+    } else if (weatherDescriptionElement.innerHTML.includes("clear")) {
+      document.getElementById("backgroundImage").style.backgroundImage =
+        "url(src/gifs/window-cat-sunny.gif)";
     }
   }
 }
@@ -93,7 +93,7 @@ function fahrenheitConverter() {
   tempElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
   let fahrenheitButton = document.querySelector(".fahrenheit");
   fahrenheitButton.style.color = "yellow";
-  fahrenheitButton.disabled = true; 
+  fahrenheitButton.disabled = true;
   let celsiusButton = document.querySelector(".celsius");
   celsiusButton.disabled = false;
 }
@@ -106,7 +106,7 @@ function celsiusConverter() {
   celsiusButton.style.color = "yellow";
   celsiusButton.disabled = true;
   let fahrenheitButton = document.querySelector(".fahrenheit");
-  fahrenheitButton.disabled = false; 
+  fahrenheitButton.disabled = false;
 }
 
 let now = new Date();
@@ -153,46 +153,18 @@ function showLocation(location) {
   getForecast(location.data.coord);
   let cityAndCountry = document.querySelector(".city-and-country");
   cityAndCountry.innerHTML = `${location.data.name}, ${location.data.sys.country}`;
-  let weatherCondition = document.querySelector(".weather-condition");
-  weatherCondition.innerHTML = `${location.data.weather[0].description}`;
-  let windElement = document.querySelector("#windspeed");
-  let windSpeed = Math.round(location.data.wind.speed);
-  windElement.innerHTML = windSpeed;
-  let humidityElement = document.querySelector("#humidity");
-  let humidity = location.data.main.humidity;
-  humidityElement.innerHTML = humidity;
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
-  
-  dateElement.innerHTML = formatDate(location.data.dt * 1000);
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/${location.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt",response.data.weather[0].description);
-  
-  if (hours < 7 || hours > 20) {
-    document.getElementById("backgroundImage").style.backgroundImage =
-      "url(src/gifs/window-cat-night.gif)";
-  } else if (weatherCondition.innerHTML.includes("ain")) {
-    document.getElementById("backgroundImage").style.backgroundImage =
-      "url(src/gifs/window-cat-rain.gif)";
-  } else if (weatherCondition.innerHTML.includes("cloud")) {
-    document.getElementById("backgroundImage").style.backgroundImage =
-      "url(src/gifs/window-cat-clouds.gif)";
-  }
-
-  
+  showTemperature(location);
 }
 
 function displayForecast(response) {
-  console.log(response.data.daily);
+  // console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row g-1 justify-content-center weekdays"><div class = "col"></div>`;
-  let days = ["Thu", "Fri","Sat","Sun","Mon"];
-  days.forEach(function(day) {
-    forecastHTML = forecastHTML +
-    `
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
             <div class="col">
               <div class="card text-info" style="background-image: url('src/images/test.jpg')">
                 <div class="card-body p-0">${day}</div>
@@ -204,7 +176,7 @@ function displayForecast(response) {
               </div>
             </div>
   `;
-});
+  });
   forecastHTML = forecastHTML + `<div class = "col-2"></div></div>`;
   forecastElement.innerHTML = forecastHTML;
 }
